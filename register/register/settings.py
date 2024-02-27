f46fd9d53210ad8ffdcb5ec0f5ce261e8817ed28
Saeed.nm <saeedmoozhikkal@gmail.com>
2024-02-27 20:26:50 +0530

    commit

--- register/register/settings.py
+++ register/register/settings.py
@@ -104,11 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Adjust as per your Redis setup
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        },
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
     }
 }
 
