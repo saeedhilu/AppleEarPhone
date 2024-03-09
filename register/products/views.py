c59111cce8a2c940c573f4a6f604088bca6fe47e
SAEED NM <150129271+saeedhilu@users.noreply.github.com>
2024-03-09 08:56:35 +0000

    before updation

--- register/products/views.py
+++ register/products/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render,redirect
 from django.views.decorators.cache import never_cache
 from .models import Products
 
-def cache_saving():
-    """
-    Fetches the products either from the cache or the database,
-    and caches the result with a timeout if not already in the cache.
-    """
-    cache_values = cache.get('cache_values')
+# def cache_saving():
+#     """
+#     Fetches the products either from the cache or the database,
+#     and caches the result with a timeout if not already in the cache.
+#     """
+#     cache_values = cache.get('cache_values')
 
-    if cache_values is None:
-        cache_values = Products.objects.all()
-        cache.set('cache_values', cache_values, timeout=30)  # Set timeout to 30 seconds
-        print("Fetched from database and cached.")
-    else:
-        print("Fetched from cache.")
+#     if cache_values is None:
+#         cache_values = Products.objects.all()
+#         cache.set('cache_values', cache_values, timeout=30)  # Set timeout to 30 seconds
+#         print("Fetched from database and cached.")
+#     else:
+#         print("Fetched from cache.")
 
-    return cache_values
+#     return cache_values
 
 
 @never_cache
@@ -31,7 +31,6 @@ def products_page(request):
 
     # r.set('key', 'redis-py')
     # r.get('key')
-
     if not request.user.is_authenticated:
             return redirect('signin')
     # Get the current value of the 'count' cookie, defaulting to 0 if it doesn't exist
@@ -39,9 +38,9 @@ def products_page(request):
     
     # Increment the count
     count += 1
-    
+    cache_values = Products.objects.all()
     # Retrieve the product list from the cache or database
-    product_list = cache_saving()
+    product_list = cache_values
     
     # Set the updated 'count' cookie in the response
     response = render(request, 'products.html', {'value': product_list, 'count': count})
